49f2264431fcb1bcfc51dc13f0256d3ed3a054d7
Victor Sánz <victor.alucard@gmail.com>
2021-03-25 22:55:50 -0300

    frontend inicio
    frontend inicio

---
+++ frontend/public/script.js
@@ -0,0 +1,6 @@
+/*
+* Babel: converter (transpilar) código do React para um código que o browser entenda
+* Webpack: Pra cada tipo de arquivo (.js, .css, .png) eu vou converter o * código de uma maneira diferente. 
+* Loaders: babel-loader, css-loader, image-loader é algo utilizado pelo 
+* webpack para dar sentido para nosso javaScript
+*/
